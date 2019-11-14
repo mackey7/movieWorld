78be3e29427ed68146edb5c860d0dae5fe911080
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-14 22:09:42 +0100

    add styles for searchBar

--- movie-world/src/components/SearchBar/SearchBar.js
+++ movie-world/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Input = styled.input`
+border-color:  #000;
+border-width: 1px 1px 1px 1px;
+padding:7px 20px;
+`
+
+const Button = styled.button`
+border-color:  #000;
+border-width: 1px 1px 1px 0px;
+background:#f5f6f7;
+padding: 7px 10px;
+margin:0;
+cursor:pointer;
+font-weight:700;
+&:hover{
+background:#ebedf0;
+
+}
+
+
+`
 export const SearchBar = () => {
     return (
         <form>
             <label for="search"></label>
-            <input type="text" name="search" id="search" />
-            <button type="submit" >
+            <Input type="text" name="search" id="search" />
+            <Button type="submit" >
                 <i className="fas fa-search"></i>
-            </button>
+            </Button>
         </form>
 
     )
