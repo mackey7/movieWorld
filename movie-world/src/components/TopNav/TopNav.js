2e9c59bc667a0bff343661f82dba75f4b0de0fd1
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-14 21:48:11 +0100

    center in horizontal logo and searchbar

--- movie-world/src/components/TopNav/TopNav.js
+++ movie-world/src/components/TopNav/TopNav.js
@@ -1,21 +1,32 @@
 import React from "react"
 import styled from 'styled-components'
 import { SearchBar } from '../../components/SearchBar/SearchBar'
+import { SimpleRow } from '../../handlers/styles'
+
+
 const TopNavWrapper = styled.nav`
 background:#384b55;
 padding:20px 0px;
+display:flex;
+justify-content:space-around;
+min-width:100%;
 `
 const Logo = styled.span`
 text-shadow: 2px 0px #fff;
 font-size:40px;
 font-weight:900;
 color:#3d7895;
+margin-right:10px;
 `
 export const TopNav = () => {
     return (
         <TopNavWrapper>
-            <Logo>Movie Word</Logo>
-            <SearchBar />
+            <SimpleRow>
+                <Logo>Movie Word</Logo>
+                <SearchBar />
+            </SimpleRow>
+
+
         </TopNavWrapper>
     )
 }
