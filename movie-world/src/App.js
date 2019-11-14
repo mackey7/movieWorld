6a18b0ca73faf7d191c9039cae97d69091fe0d10
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-14 20:49:59 +0100

    create route for homepage container

--- movie-world/src/App.js
+++ movie-world/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { HomePage } from './containers/HomePage/HomePage'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <div >
-      <HomePage />
-    </div>
+    <Router>
+      <div >
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
