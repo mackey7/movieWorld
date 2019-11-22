74d32a44fc0c6cd309696f8054ab4f8382bd7be1
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-22 22:38:20 +0100

    create structure for MovieItem pages and create route

--- movie-world/src/App.js
+++ movie-world/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HomePage } from './Pages/HomePage/HomePage'
 import { Favourite } from './Pages/Favourite/Favourite'
-
+import { MovieItem } from './Pages/MovieItem/MovieItem'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <div >
           <Route exact path="/" component={HomePage} />
           <Route exact path="/Favourite" component={Favourite} />
-
+          <Route exact path="/movie/:id" component={MovieItem} />
         </div >
 
       </Router>
