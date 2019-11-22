9e76bc6ed89afc4af019c910780b6187ca345550
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-22 21:56:34 +0100

    create structure for header component

--- movie-world/src/containers/HomePage/HomePage.js
+++ movie-world/src/containers/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { TopNav } from '../../components/TopNav/TopNav'
 import { Footer } from '../../components/Footer/Footer'
+import { Header } from '../../components/Header/Header'
 export const HomePage = () => {
     return (
         <div>
             <TopNav />
+            <Header />
             <Footer />
+
         </div>
     )
 }
