5fb5da1144d9fbd8ab095efa56eb7a58eb6ec0f7
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-26 21:45:35 +0100

    add style

--- movie-world/src/components/Footer/Footer.js
+++ movie-world/src/components/Footer/Footer.js
@@ -1,27 +1,51 @@
 import React from 'react'
 import Styled from 'styled-components'
 
-const FooterWrapper = Styled.footer`
-
+const FooterContainer = Styled.footer`
+background: #131517;
+padding:20px 0px;
+min-width:100%;
 `
 
+const FooterWrapper = Styled.div`
+width:100%;
+color:#fff;
+display:flex;
+justify-content: space-around;
+align-items:center;
+`
+const Logo = Styled.span`
+font-size:30px;
+font-weight:900;
+`
+
+const Icon = Styled.i`
+font-size:26px;
+padding-left:20px;
+color:#b9b9ba;
+cursor:pointer;
+
+&:hover{
+    color:#fff;
+}
+
+`
 export const Footer = () => {
 
     return (
+        <FooterContainer>
+            <FooterWrapper>
+                <Logo>
+                    Movie World
 
-        <FooterWrapper>
-            <div>
-                Movie World
+                </Logo>
+                <div>
+                    <Icon className="fab fa-twitter"></Icon>
+                    <Icon className="fab fa-facebook"></Icon>
+                </div>
 
-            </div>
-            <div>
-                <i className="fab fa-twitter"></i>
-                <i className="fab fa-facebook"></i>
-
-            </div>
-
-
-        </FooterWrapper>
+            </FooterWrapper>
+        </FooterContainer>
 
     )
 }
