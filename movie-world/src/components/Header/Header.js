9a21dcf9c32d08d5d8e90da1f1b4d7bbe2728b9a
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-27 21:35:00 +0100

    create slides component

--- movie-world/src/components/Header/Header.js
+++ movie-world/src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { Slides } from './Slides'
 export const Header = () => {
     return (
 
         <div>
-            Header
+            <Slides />
+
         </div>
     )
 }
---
+++ movie-world/src/components/Header/Slides.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Slides = () => {
+    return (
+        <div>
+            Slides
+        </div>
+    )
+}
