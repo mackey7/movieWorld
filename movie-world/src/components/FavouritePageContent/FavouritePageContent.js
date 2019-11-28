0a5753235444ca40661e85f591e0046b2ecfa015
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-28 23:01:44 +0100

    create style for component

--- movie-world/src/components/FavouritePageContent/FavouritePageContent.js
+++ movie-world/src/components/FavouritePageContent/FavouritePageContent.js
@@ -15,12 +15,23 @@ display:flex;
 flex-wrap:wrap;
 justify-content:flex-start;
 `
+const Title = Styled.h2`
+text-align:center;
+color:#fff;
+font-size:40px;
+background:#eb4e7a;
+padding:10px;
+font-weight:100;
 
+`
 export const FavouritePageContent = () => {
     return (
         <Wrapper>
+            <Title> Favourite</Title>
             <MainWrapper>
-                <  FavouritePageMovieItem />
+
+                <  FavouritePageMovieItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
+                <  FavouritePageMovieItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
             </MainWrapper>
 
         </Wrapper>
--- movie-world/src/components/FavouritePageMovieItem/FavouritePageMovieItem.js
+++ movie-world/src/components/FavouritePageMovieItem/FavouritePageMovieItem.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import Styled from 'styled-components'
+import { Link } from 'react-router-dom'
+const Wrapper = Styled.div`
+display:flex;
+flex-direction:column;
+cursor:pointer;
+margin: 0px 20px;
+color:#d8d8d8;
+text-decoration:none;
+&:hover{
+    transform:scale(1.1) ;
+    color:#fff;
+}
+`
+
+const Image = Styled.img`
+margin-bottom:5px;
+filter: brightness(50%);
+&:hover{
+ filter: brightness(100%); 
+}
+`
+
+const MovieName = Styled.span`
+font-size:14px;;
+text-decoration:none;
+text-align: left;
+`
+
+const MovieCategory = Styled.span`
+font-size:11px ;
+text-decoration:none;
+text-align: left;
+`
 
 
-export const FavouritePageMovieItem = () => {
+export const FavouritePageMovieItem = ({ src, name, category }) => {
     return (
-        <div>
-            FavouritePageMovieItem
-        </div>
+        <Link to="/movie/8">
+            <Wrapper>
+
+                <Image src={src} />
+                <MovieName>
+                    {name}
+                </MovieName>
+                <MovieCategory>
+                    {category}
+                </MovieCategory>
+            </Wrapper>
+        </Link>
     )
 }
