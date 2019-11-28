de2463feab4186228e07aec7a43412ade614645e
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-28 21:52:51 +0100

    add background and display flex

--- movie-world/src/components/FavouritePageContent/FavouritePageContent.js
+++ movie-world/src/components/FavouritePageContent/FavouritePageContent.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import Styled from 'styled-components'
 
+const Wrapper = Styled.section`
+min-height:100vh;
+background:#111617;
+border-bottom:1px solid #fff;
+border-top:1px solid #fff;
+`
+const MainWrapper = Styled.section`
+width:80%;
+margin: 0 auto;
+padding:20px 0px;
+display:flex;
+flex-wrap:wrap;
+justify-content:flex-start;
+`
 
 export const FavouritePageContent = () => {
     return (
-        <div>
+        <Wrapper>
+            <MainWrapper>
+                FavouritePageContent
+            </MainWrapper>
 
-            FavouritePageContent
-    </div>
+        </Wrapper>
     )
 }
