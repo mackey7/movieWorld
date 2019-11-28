a23589dec74b2a51e43d82c5e932472707d9ec5d
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-28 20:56:10 +0100

    Create FavouritePageContent component

--- movie-world/src/Pages/Favourite/Favourite.js
+++ movie-world/src/Pages/Favourite/Favourite.js
@@ -1,9 +1,13 @@
 import React from "react"
-
+import { TopNav } from '../../components/TopNav/TopNav'
+import { Footer } from '../../components/Footer/Footer'
+import { FavouritePageContent } from '../../components/FavouritePageContent/FavouritePageContent'
 export const Favourite = () => {
     return (
         <div>
-            Favourite
+            <TopNav />
+            <FavouritePageContent />
+            <Footer />
         </div>
     )
 }
---
+++ movie-world/src/components/FavouritePageContent/FavouritePageContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const FavouritePageContent = () => {
+    return (
+        <div>
+
+            FavouritePageContent
+    </div>
+    )
+}
