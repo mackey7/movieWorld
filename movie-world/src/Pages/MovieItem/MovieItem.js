d7a48d6e6499003ee1caca46ef38fd00aa5650be
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-12-02 21:43:46 +0100

    create MovieItemSummary file

--- movie-world/src/Pages/MovieItem/MovieItem.js
+++ movie-world/src/Pages/MovieItem/MovieItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { MovieItemHeader } from '../../components/MovieItemHeader/MovieItemHeader'
+import { MovieItemSummary } from '../../components/MovieItemSummary/MovieItemSummary'
 export const MovieItem = () => {
     return (
         <div>
             <MovieItemHeader />
+            <MovieItemSummary />
         </div>
     )
 }
---
+++ movie-world/src/components/MovieItemSummary/MovieItemSummary.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MovieItemSummary = () => {
+    return (
+        <div>
+            MovieItemSummary
+    </div>
+    )
+}
