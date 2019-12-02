6784cf1f5c4e3e2770833cb454e4bea4bfdbca60
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-12-02 23:11:11 +0100

    create background header

--- movie-world/src/components/MovieItemHeader/MovieItemHeader.js
+++ movie-world/src/components/MovieItemHeader/MovieItemHeader.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Styled from 'styled-components'
+
+const Wrapper = Styled.header`
+min-width:100%;
+height:100vh;
+background-image: url("https://image.tmdb.org/t/p/original/k2WyDw2NTUIWnuEs5gT7wgrCQg6.jpg");
+background-repeat:none;
+background-position:center;
+background-size: 100% 100vh; 
+
+`
 
 export const MovieItemHeader = () => {
     return (
-        <div>
+        <Wrapper>
 
             MovieItemHeader
-        </div>
+        </Wrapper>
     )
 }
