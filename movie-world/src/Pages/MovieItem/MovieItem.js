bfa2080fe1d5d264be979ea79e6d9095fcde5410
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-12-02 21:45:46 +0100

    create MovieItemCast file

--- movie-world/src/Pages/MovieItem/MovieItem.js
+++ movie-world/src/Pages/MovieItem/MovieItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { MovieItemHeader } from '../../components/MovieItemHeader/MovieItemHeader'
 import { MovieItemSummary } from '../../components/MovieItemSummary/MovieItemSummary'
+import { MovieItemCast } from '../../components/MovieItemCast/MovieItemCast'
 export const MovieItem = () => {
     return (
         <div>
             <MovieItemHeader />
             <MovieItemSummary />
+            <MovieItemCast />
         </div>
     )
 }
