7d55e64d08d6127eb5b5cd493bb441c2cf5d0fba
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-12-02 22:02:23 +0100

    create MovieItemTrailers file

--- movie-world/src/Pages/MovieItem/MovieItem.js
+++ movie-world/src/Pages/MovieItem/MovieItem.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { MovieItemHeader } from '../../components/MovieItemHeader/MovieItemHeader'
 import { MovieItemSummary } from '../../components/MovieItemSummary/MovieItemSummary'
 import { MovieItemCast } from '../../components/MovieItemCast/MovieItemCast'
+import { MovieItemTrailers } from '../../components/MovieItemTrailers/MovieItemTrailers'
 export const MovieItem = () => {
     return (
         <div>
             <MovieItemHeader />
             <MovieItemSummary />
             <MovieItemCast />
+            <MovieItemTrailers />
         </div>
     )
 }
---
+++ movie-world/src/components/MovieItemTrailers/MovieItemTrailers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MovieItemTrailers = () => {
+    return (
+        <div>
+            MovieItemTrailers
+        </div>
+    )
+}
