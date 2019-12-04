16fcec944d2a8844bf0db1a2f84489495ee6d22f
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-12-04 23:03:51 +0100

    add style for  MovieItemSummary component

--- movie-world/src/components/MovieItemSummary/MovieItemSummary.js
+++ movie-world/src/components/MovieItemSummary/MovieItemSummary.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Styled from 'styled-components'
+
+const WrapperSection = Styled.section`
+background: #161130;
+width:100%;
+height:100%;
+padding:100px 0px 30px 0px;
+color:#fff;
+`
+const Wrapper = Styled.section`
+width:80%;
+margin: 0 auto;
+`
+
+const Title = Styled.h2`
+text-transform:uppercase;
+`
+
 
 export const MovieItemSummary = () => {
     return (
-        <div>
-            MovieItemSummary
-    </div>
+        <WrapperSection>
+            <Wrapper>
+
+                <Title>
+                    Summary
+                </Title>
+                <p>
+                    Elsa, Anna, Kristoff and Olaf head far into the forest to learn the truth about an ancient mystery of their kingdom.
+                    Elsa, Anna, Kristoff and Olaf head far into the forest to learn the truth about an ancient mystery of their kingdom.
+                    Elsa, Anna, Kristoff and Olaf head far into the forest to learn the truth about an ancient mystery of their kingdom.
+
+                </p>
+
+            </Wrapper>
+        </WrapperSection>
     )
 }
