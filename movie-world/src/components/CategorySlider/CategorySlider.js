ad853234e9de9f2a2f488b9f3da891a29baa44ff
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-12-05 23:06:14 +0100

    add animations for categorySlider component

--- movie-world/src/components/CategorySlider/CategorySlider.js
+++ movie-world/src/components/CategorySlider/CategorySlider.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Styled from 'styled-components'
 import { CategorySliderFilmItem } from '../CategorySliderFilmItem/CategorySliderFilmItem'
-
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const SliderContainer = Styled.section`
 background:#3d2f87;
@@ -38,21 +38,36 @@ export const CategorySlider = ({ CategoryTitle }) => {
     return (
         <SliderContainer>
             <SliderWrapper>
-                <CategoryName>
-                    {CategoryTitle}
-                </CategoryName>
+                <ScrollAnimation animateIn="bounceInLeft" animateOnce={true}>
+                    <CategoryName>
+                        {CategoryTitle}
+                    </CategoryName>
+                </ScrollAnimation>
                 <MainConatiner>
-                    <Icon className="fas fa-chevron-left"></Icon>
+                    <ScrollAnimation animateIn="bounceInLeft" animateOnce={true}>
+                        <Icon className="fas fa-chevron-left"></Icon>
+                    </ScrollAnimation>
                     <Slider>
-                        < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
-                        < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
-                        < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
-                        < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
+                        <ScrollAnimation animateIn="fadeInUpBig" animateOnce={true}>
+                            < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn="fadeInUpBig" animateOnce={true}>
+                            < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn="fadeInUpBig" animateOnce={true}>
+                            < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn="fadeInUpBig" animateOnce={true}>
+                            < CategorySliderFilmItem src="https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg" category="Animation | Adventure" name=" Barbie: Dolphin Magic" />
+                        </ScrollAnimation>
+
                     </Slider>
-                    <Icon className="fas fa-chevron-right"></Icon>
+                    <ScrollAnimation animateIn="bounceInRight" animateOnce={true}>
+                        <Icon className="fas fa-chevron-right"></Icon>
+                    </ScrollAnimation>
                 </MainConatiner>
 
             </SliderWrapper>
-        </SliderContainer>
+        </SliderContainer >
     )
 }
