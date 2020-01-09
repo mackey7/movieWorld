eb1aaaed7d9f74e26892eeea3ab120ff5f6f256e
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-10 00:26:04 +0100

    :connect  with carusel

--- movie-world/src/containers/HomePageCategories/HomePageCategories.js
+++ movie-world/src/containers/HomePageCategories/HomePageCategories.js
@@ -1,27 +1,13 @@
 import React from 'react'
 import { CategorySlider } from '../../components/CategorySlider/CategorySlider'
-import { fetchUpcomingMovie, fetchPopularMovie } from '../../actions/movies'
+import { fetchUpcomingMovie, fetchPopularMovie, fetchNowPlayingMovies } from '../../actions/movies'
 import { connect } from "react-redux";
 
 class HomePageCategories extends React.Component {
-    MovieArray = [
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" },
-        { 'poster_path': "https://image.tmdb.org/t/p/w154/r15SUgzjL8bablcdEkHk9T7TSRl.jpg", "media_type": "Animation | Adventure", "title": "Barbie: Dolphin Magic" }
-
-    ]
     componentDidMount() {
         this.props.fetchUpcomingMovie();
-        this.props.fetchPopularMovie()
+        this.props.fetchPopularMovie();
+        this.props.fetchNowPlayingMovies()
     }
 
     render() {
@@ -29,8 +15,8 @@ class HomePageCategories extends React.Component {
             <div>
                 <CategorySlider CategoryTitle={"Upcoming"} data={this.props.popular_movies.results} />
                 <CategorySlider CategoryTitle={"Popular"} data={this.props.upcoming_movie.results} />
-                <CategorySlider CategoryTitle={"Now Playing"} data={this.MovieArray} />
-                <CategorySlider CategoryTitle={"Top Rated"} data={this.MovieArray} />
+                <CategorySlider CategoryTitle={"Now Playing"} data={this.props.now_playing_movies.results} />
+                {/* <CategorySlider CategoryTitle={"Top Rated"} data={this.MovieArray} /> */}
             </div >
         )
     }
@@ -40,13 +26,15 @@ const mapDispatchToProps = (dispatch) => {
     return {
         fetchUpcomingMovie: () => dispatch(fetchUpcomingMovie()),
         fetchPopularMovie: () => dispatch(fetchPopularMovie()),
+        fetchNowPlayingMovies: () => dispatch(fetchNowPlayingMovies()),
     }
 }
 const mapStateToProps = (state) => {
     console.log(state.movies)
     return {
         upcoming_movie: state.movies.upcoming_movie,
-        popular_movies: state.movies.popular_movies
+        popular_movies: state.movies.popular_movies,
+        now_playing_movies: state.movies.now_playing_movies
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(HomePageCategories)
