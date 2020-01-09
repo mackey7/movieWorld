a15f4f9f0cf9764d8ccef4c7d2f3c692d73422ec
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-10 00:29:53 +0100

    create top rated movies reducer

--- movie-world/src/reducers/moviesReducer.js
+++ movie-world/src/reducers/moviesReducer.js
@@ -1,9 +1,10 @@
-import { FETCH_UPCOMING_MOVIE, FETCH_POPULAR_MOVIES, FETCH_NOW_PLAYING_MOVIES } from '../actions/actions_types'
+import { FETCH_UPCOMING_MOVIE, FETCH_POPULAR_MOVIES, FETCH_NOW_PLAYING_MOVIES, FETCH_TOP_RATED_MOVIES } from '../actions/actions_types'
 
 const initial = {
     upcoming_movies: [],
     popular_movies: [],
     now_playing_movies: [],
+    top_rated_movies: []
 
 }
 
@@ -17,6 +18,9 @@ const moviesReducer = (state = initial, actions) => {
         }
         case FETCH_NOW_PLAYING_MOVIES: {
             return { ...state, now_playing_movies: actions.payload }
+        }
+        case FETCH_TOP_RATED_MOVIES: {
+            return { ...state, top_rated_movies: actions.payload }
         }
         default:
             return state;
