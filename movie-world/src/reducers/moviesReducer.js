8da903c24c78addc448f5b45875a48a8094369fd
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-10 00:23:37 +0100

    :create now playing reducer

--- movie-world/src/reducers/moviesReducer.js
+++ movie-world/src/reducers/moviesReducer.js
@@ -1,22 +1,22 @@
-import { FETCH_POPULAR_MOVIE_OF_DAY, FETCH_UPCOMING_MOVIE, FETCH_POPULAR_MOVIES } from '../actions/actions_types'
+import { FETCH_UPCOMING_MOVIE, FETCH_POPULAR_MOVIES, FETCH_NOW_PLAYING_MOVIES } from '../actions/actions_types'
 
 const initial = {
-    films: [],
-    upcoming_movie: [],
-    popular_movies: []
+    upcoming_movies: [],
+    popular_movies: [],
+    now_playing_movies: [],
 
 }
 
 const moviesReducer = (state = initial, actions) => {
     switch (actions.type) {
-        case FETCH_POPULAR_MOVIE_OF_DAY: {
-            return { ...state, films: actions.payload }
-        }
         case FETCH_UPCOMING_MOVIE: {
             return { ...state, upcoming_movie: actions.payload }
         }
         case FETCH_POPULAR_MOVIES: {
             return { ...state, popular_movies: actions.payload }
+        }
+        case FETCH_NOW_PLAYING_MOVIES: {
+            return { ...state, now_playing_movies: actions.payload }
         }
         default:
             return state;
