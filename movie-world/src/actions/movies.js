9e274e245d72851ad732a951aea6f514412f4036
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-09 23:29:39 +0100

    create action for fetch upcoming movie

--- movie-world/src/actions/movies.js
+++ movie-world/src/actions/movies.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { FETCH_POPULAR_MOVIE_OF_DAY } from './actions_types/index'
+import { FETCH_POPULAR_MOVIE_OF_DAY, FETCH_UPCOMING_MOVIE } from './actions_types/index'
 import { API_KEY } from '../env/API_KEY'
 const API = "https://api.themoviedb.org/3/trending/movie/day?api_key="
 
@@ -18,6 +18,25 @@ export const fetchPopularMovieOfDayApi = () => {
 export const fetchPopularMovieOfDay = (payload) => {
     return {
         type: FETCH_POPULAR_MOVIE_OF_DAY,
+        payload
+    }
+}
+
+export const fetchUpcomingMovie = () => {
+    return (dispatch) => {
+        return axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1 `)
+            .then(response => {
+                dispatch(fetchUpcomingMovieSuccess(response.data));
+            })
+            .catch(error => {
+                throw error;
+            })
+    }
+}
+
+export const fetchUpcomingMovieSuccess = (payload) => {
+    return {
+        type: FETCH_UPCOMING_MOVIE,
         payload
     }
 }
