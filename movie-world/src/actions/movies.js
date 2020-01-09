108b92440199e129aa6350c8f63753f664c61a2d
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-10 00:21:45 +0100

    create now playing movies action

--- movie-world/src/actions/movies.js
+++ movie-world/src/actions/movies.js
@@ -1,26 +1,9 @@
 import axios from 'axios'
-import { FETCH_POPULAR_MOVIE_OF_DAY, FETCH_UPCOMING_MOVIE, FETCH_POPULAR_MOVIES } from './actions_types/index'
+import { FETCH_UPCOMING_MOVIE, FETCH_POPULAR_MOVIES, FETCH_NOW_PLAYING_MOVIES } from './actions_types/index'
 import { API_KEY } from '../env/API_KEY'
-const API = "https://api.themoviedb.org/3/trending/movie/day?api_key="
 
-export const fetchPopularMovieOfDayApi = () => {
-    return (dispatch) => {
-        return axios.get(`${API}${API_KEY} `)
-            .then(response => {
-                dispatch(fetchPopularMovieOfDay(response.data));
-            })
-            .catch(error => {
-                throw error;
-            })
-    }
-}
 
-export const fetchPopularMovieOfDay = (payload) => {
-    return {
-        type: FETCH_POPULAR_MOVIE_OF_DAY,
-        payload
-    }
-}
+
 
 export const fetchUpcomingMovie = () => {
     return (dispatch) => {
@@ -55,6 +38,24 @@ export const fetchPopularMovie = () => {
 export const fetchPopularMovieSuccess = (payload) => {
     return {
         type: FETCH_POPULAR_MOVIES,
+        payload
+    }
+}
+export const fetchNowPlayingMovies = () => {
+    return (dispatch) => {
+        return axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1 `)
+            .then(response => {
+                dispatch(fetchNowPlayingMovieSuccess(response.data));
+            })
+            .catch(error => {
+                throw error;
+            })
+    }
+}
+
+export const fetchNowPlayingMoviesSuccess = (payload) => {
+    return {
+        type: FETCH_NOW_PLAYING_MOVIES,
         payload
     }
 }
