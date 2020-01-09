13d1f49c181dd07c2899f51047afe54348b89686
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-09 23:32:36 +0100

    create upcoming case in reducer

--- movie-world/src/reducers/moviesReducer.js
+++ movie-world/src/reducers/moviesReducer.js
@@ -1,13 +1,18 @@
-import { FETCH_POPULAR_MOVIE_OF_DAY } from '../actions/actions_types'
+import { FETCH_POPULAR_MOVIE_OF_DAY, FETCH_UPCOMING_MOVIE } from '../actions/actions_types'
 
 const initial = {
-    films: []
+    films: [],
+    upcoming_movie: [],
+
 }
 
 const moviesReducer = (state = initial, actions) => {
     switch (actions.type) {
         case FETCH_POPULAR_MOVIE_OF_DAY: {
             return { ...state, films: actions.payload }
+        }
+        case FETCH_UPCOMING_MOVIE: {
+            return { ...state, upcoming_movie: actions.payload }
         }
         default:
             return state;
