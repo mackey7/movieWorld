a0d114ad182e4bcd974d0db94ab5271ea368e9ac
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-15 20:53:39 +0100

    fix bug in carusel and add style for carusel

--- movie-world/src/components/CategorySlider/CategorySlider.js
+++ movie-world/src/components/CategorySlider/CategorySlider.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import Styled from 'styled-components'
 import { CategorySliderFilmItem } from '../CategorySliderFilmItem/CategorySliderFilmItem'
-import ScrollAnimation from 'react-animate-on-scroll';
-// import Swiper from 'react-id-swiper';
+
 import Swiper from 'swiper';
 import 'swiper/css/swiper.css'
-import { Link } from 'react-router-dom'
+
 import '../../styles/carousel.scss'
 
 const SliderContainer = Styled.section`
@@ -19,78 +18,92 @@ const SliderWrapper = Styled.div`
     max-width:80%;
 `
 
-
-
-const Slider = Styled.div`
-    display:flex;
-    flex-direction:row;
-    max-width:80%;
-    margin: 40px auto;
-`
-
-const CC = Styled.div`
-color:red;
-`
 const CategoryName = Styled.span`
-    color:#fff;
+    text-transform:uppercase;  
+     color:#e1e1e1;
+    font-weight:100;
+    letter-spacing:2px;
+`
+const Loader = Styled.div`
+    margin:0 auto;
+    color:#e1e1e1;
+    letter-spacing:5px;
+    font-weight:100;
+`
+const SeparateLine = Styled.div`
+margin:50px auto;
+width:70%;
+height:2px;
+background: rgb(255,255,255);
+background: linear-gradient(90deg, rgba(255,255,255,0.24693627450980393) 0%, rgba(136,136,136,1) 52%, rgba(255,255,255,0.19371498599439774) 100%);
 `
 
 class CategorySlider extends React.Component {
     render() {
         (() => {
-            const sliderEl = document.querySelectorAll('.swiper-container');
-            if (!sliderEl) {
-                return;
-            }
-            const slider = new Swiper(sliderEl, {
-                init: true,
-                slidesPerView: 2,
-                loop: true,
-                spaceBetween: 14,
-                observer: true,
+            const sliderEl = document.querySelectorAll(".swiper-container");
 
-                breakpoints: {
-                    1145: {
-                        slidesPerView: 7
+            sliderEl.forEach((slides, index) => {
+                const slider = new Swiper(slides, {
+                    init: true,
+                    loop: true,
+                    spaceBetween: 10,
+                    observer: true,
+
+                    breakpoints: {
+                        1145: {
+                            slidesPerView: 8
+                        },
+                        699: {
+                            slidesPerView: 3
+                        }
                     },
-                    699: {
-                        slidesPerView: 3
-                    },
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                }
+                    navigation: {
+                        nextEl: `.swiper-button-next${index}`,
+                        prevEl: `.swiper-button-prev${index}`
+                    }
+                });
             });
         })();
+
         return (
-            < SliderContainer>
-                <div className="carousel-container wow fadeIn" data-wow-duration="3s">
-                    <CategoryName> {this.props.CategoryTitle}</CategoryName>
-                    <div className="swiper-container">
-                        <div className="swiper-wrapper">
-                            {this.props.data !== undefined && this.props.data.length > 0 ?
-                                this.props.data.map((item, key) => {
-                                    return (
-                                        <div key={key} className="swiper-slide">
-                                            <Link to={`/movie/${item.id}`} >
-                                                < CategorySliderFilmItem src={item.poster_path} category={item.media_type} name={item.title} />
-                                            </Link>
-                                        </div >
-                                    )
-                                }) : <p>LOADING...</p>
-                            }
+            <SliderContainer>
+                <SliderWrapper>
+                    <div className="carousel-container" >
+                        <CategoryName> {this.props.CategoryTitle}</CategoryName>
+                        <div className="swiper-container">
+                            <div className="swiper-wrapper">
+                                {this.props.data !== undefined && this.props.data.length > 0 ? (
+                                    this.props.data.map((item, key) => {
+                                        return (
+                                            <div key={key} className="swiper-slide">
+                                                <CategorySliderFilmItem
+                                                    src={item.poster_path}
+                                                    category={item.media_type}
+                                                    name={item.title}
+                                                    id={item.id}
+                                                />
+                                            </div>
+                                        );
+                                    })
+                                ) : (
+                                        <Loader>Loading...</Loader>
+                                    )}
+                            </div>
                         </div>
+
+                        <div
+                            className={`swiper-button-prev${this.props.indexOfSwiper}`}
+                        ></div>
+                        <div
+                            className={`swiper-button-next${this.props.indexOfSwiper}`}
+                        ></div>
                     </div>
-
-                    <div className="swiper-button-prev"></div>
-                    <div className="swiper-button-next"></div>
-
-                </div>
-            </SliderContainer>
-
-        )
+                </SliderWrapper>
+                <SeparateLine> </SeparateLine>
+            </SliderContainer >
+        );
     }
 }
 
-export default CategorySlider
+export default CategorySlider;
--- movie-world/src/components/CategorySliderFilmItem/CategorySliderFilmItem.js
+++ movie-world/src/components/CategorySliderFilmItem/CategorySliderFilmItem.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import Styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+
+const FilmLink = Styled(Link)`
+    text-decoration:none;
+
+`
 
 const Wrapper = Styled.div`
     display:flex;
+   height:100%;
     flex-direction:column;
     cursor:pointer;
     margin: 0px 20px;
+       filter: brightness(50%);
+        &:hover{
+        filter: brightness(100%);   
+      transform:scale(1.1);
+      transition-duration: 700ms;
+transition-property: margin-right;
+        }
 `
 
 const Image = Styled.img`
+    max-width:100px;
     margin-bottom:5px;
-    filter: brightness(50%);
-        &:hover{
-        filter: brightness(100%);   
-        }
+    margin:0 auto;
 `
 
 const MovieName = Styled.span`
     font-size:14px;;
     color:#fff;
-    text-align: left;
+    text-align: center;
+    padding-top:2px;
 `
 
 const MovieCategory = Styled.span`
@@ -28,18 +42,19 @@ const MovieCategory = Styled.span`
     text-align: left;
 `
 
-export const CategorySliderFilmItem = ({ src, name, category }) => {
+export const CategorySliderFilmItem = ({ src, name, category, id }) => {
     return (
-
-        <Wrapper>
-            <Image src={`https://image.tmdb.org/t/p/w154/${src}`} />
-            <MovieName>
-                {name}
-            </MovieName>
-            <MovieCategory>
-                {category}
-            </MovieCategory>
-        </Wrapper>
+        <FilmLink to={`/movie/${id}`}>
+            <Wrapper>
+                <Image src={`https://image.tmdb.org/t/p/w154/${src}`} />
+                <MovieName>
+                    {name}
+                </MovieName>
+                <MovieCategory>
+                    {category}
+                </MovieCategory>
+            </Wrapper>
+        </FilmLink>
 
     )
 }
