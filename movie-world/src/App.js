00e831fadca714162fd1414d179ac8750de1af39
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-17 22:11:00 +0100

    create search route

--- movie-world/src/App.js
+++ movie-world/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HomePage } from './Pages/HomePage/HomePage'
 import { Favourite } from './Pages/Favourite/Favourite'
+import { Search } from './Pages/Search/Search'
 import MovieItem from './Pages/MovieItem/MovieItem'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/Favourite" component={Favourite} />
           <Route exact path="/movie/:id" component={MovieItem} />
+          <Route exact path="/search/:id" component={Search} />
         </div >
 
       </Router>
