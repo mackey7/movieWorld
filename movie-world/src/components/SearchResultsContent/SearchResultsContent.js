46738b34974cd87fe6a36a12143d58839d066e3e
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-20 21:50:26 +0100

    add styled to  components

--- movie-world/src/components/SearchResultsContent/SearchResultsContent.js
+++ movie-world/src/components/SearchResultsContent/SearchResultsContent.js
@@ -1,13 +1,54 @@
 import React from 'react'
+import Styled from 'styled-components'
 
+const SectionWrapper = Styled.section`
+    background:#3d2f87;
+    min-width:100%;
+    min-height:100vh;
+    padding:0;
+    margin:0;
+`
+const PageTitle = Styled.h1`
+    text-align:center;
+    font-weight:100;
+    text-transform:uppercase;
+    padding:20px;
+    color:#d8d8d8;
+    margin:0;
+`
 
-export const SearchResultsContent = () => {
+const Content = Styled.div`
+    margin:0 auto;
+`
+
+export const SearchResultsContent = ({ url, data }) => {
 
     return (
-        <div>
-            ss
-        </div>
+        <SectionWrapper>
+            <PageTitle> Search results for {url} </PageTitle>
+            <Content>
+                {
+                    data ?
+                        data.map((item, key) =>
+                            <div key={key}>
+
+                            </div>
+                        )
+                        :
+                        <div> NO RESLUTS</div>
+                }
+
+            </Content>
+        </SectionWrapper>
     )
 
 
 }
+
+
+// const SliderWrapper = Styled.div`
+//    
+//     font-size:24px;
+//     font-weight:700;
+//     max-width:80%;
+// `
