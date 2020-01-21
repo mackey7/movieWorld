d22c8ebe1d9ccff9102b483360383d82e6017b6c
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-21 22:38:06 +0100

    fix handleadd function

--- movie-world/src/components/MovieItemHeader/MovieItemHeader.js
+++ movie-world/src/components/MovieItemHeader/MovieItemHeader.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Styled from 'styled-components'
 import { Animated } from "react-animated-css";
 
@@ -58,37 +58,49 @@ color:#fff;
 
 
 
-export const MovieItemHeader = ({ data, addTofavourite }) => {
-    const historyBack = () => {
+class MovieItemHeader extends Component {
+    historyBack = () => {
         window.history.back();
     }
-    return (
-        <Wrapper bgUrl={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}>
-            <HeaderContent>
-                <div>
-                    <Animated animationIn="bounceInLeft" >
-                        <Icon onClick={historyBack} className="fas fa-chevron-left"></Icon>
-                    </Animated>
-                </div>
-                <HeaderContentBottom>
-                    <MovieDetailsContainer>
-                        <Animated animationIn="bounceInLeft" >
-                            <MoviePoster src={`https://image.tmdb.org/t/p/w154/${data.poster_path}`} />
-                        </Animated>
-                        <Animated animationIn="bounceInRight" >
-                            <MovieDetails>
-                                <h2> {data.original_title}</h2>
-                                <span> {data.vote_average}</span>
-                                <span>{data.status} | {data.original_language}</span>
-                            </MovieDetails>
-                        </Animated>
 
-                    </MovieDetailsContainer>
-                    <Animated animationIn="bounceInRight" >
-                        <HeartIcon onClick={addTofavourite} className="fas fa-heart"></HeartIcon>
-                    </Animated>
-                </HeaderContentBottom>
-            </HeaderContent>
-        </Wrapper >
-    )
+    handleAdd = () => {
+        // addTofavourite(this.props.data.id)
+    }
+    render() {
+
+
+
+
+        return (
+            <Wrapper bgUrl={`https://image.tmdb.org/t/p/original/${this.props.data.backdrop_path}`}>
+                <HeaderContent>
+                    <div>
+                        <Animated animationIn="bounceInLeft" >
+                            <Icon onClick={this.historyBack} className="fas fa-chevron-left"></Icon>
+                        </Animated>
+                    </div>
+                    <HeaderContentBottom>
+                        <MovieDetailsContainer>
+                            <Animated animationIn="bounceInLeft" >
+                                <MoviePoster src={`https://image.tmdb.org/t/p/w154/${this.props.data.poster_path}`} />
+                            </Animated>
+                            <Animated animationIn="bounceInRight" >
+                                <MovieDetails>
+                                    <h2> {this.props.data.original_title}</h2>
+                                    <span> {this.props.data.vote_average}</span>
+                                    <span>{this.props.data.status} | {this.props.data.original_language}</span>
+                                </MovieDetails>
+                            </Animated>
+
+                        </MovieDetailsContainer>
+                        <Animated animationIn="bounceInRight" >
+                            <HeartIcon onClick={() => this.handleAdd()} className="fas fa-heart"></HeartIcon>
+                        </Animated>
+                    </HeaderContentBottom>
+                </HeaderContent>
+            </Wrapper >
+        )
+    }
 }
+
+export default MovieItemHeader 
--- movie-world/src/containers/MovieItemContainer/MovieItemContainer.js
+++ movie-world/src/containers/MovieItemContainer/MovieItemContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { MovieItemHeader } from '../../components/MovieItemHeader/MovieItemHeader'
+import MovieItemHeader from '../../components/MovieItemHeader/MovieItemHeader'
 import { MovieItemSummary } from '../../components/MovieItemSummary/MovieItemSummary'
 import { MovieItemCast } from '../../components/MovieItemCast/MovieItemCast'
 import { MovieItemTrailers } from '../../components/MovieItemTrailers/MovieItemTrailers'
