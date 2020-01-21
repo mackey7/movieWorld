27e0ae44f34157e47f612914aadbaaf5eb178b3f
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-21 22:09:12 +0100

    add style for search result item component

--- movie-world/src/components/SearchResultsItem/SearchResultsItem.js
+++ movie-world/src/components/SearchResultsItem/SearchResultsItem.js
@@ -6,10 +6,17 @@ const Wrapper = Styled(Link)`
 text-decoration:none;
 color:#fff;
 padding:20px;
+display:flex;
+flex-direction:column;
+justify-content:center;
 `
 
 const Img = Styled.img`
 max-height:200px;
+`
+
+const Title = Styled.p`
+text-align:center;
 `
 
 export const SearchResultsItem = ({ id, src, title }) => {
@@ -17,7 +24,7 @@ export const SearchResultsItem = ({ id, src, title }) => {
         <Wrapper to={`/movie/${id}`}>
 
             <Img src={`https://image.tmdb.org/t/p/original/${src}`} />
-            <p>{title} </p>
+            <Title>{title} </Title>
 
         </Wrapper >
 
