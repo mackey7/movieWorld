67913a1520c79086e878a830c9c5c4ce855ecdf7
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-22 22:50:22 +0100

    connect store with localstorage

--- movie-world/src/store/store.js
+++ movie-world/src/store/store.js
@@ -1,6 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/rootReducer'
 import thunk from 'redux-thunk';
-const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const persistedState = loadState();
+
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk))
+
+store.subscribe(() => {
+    saveState({
+        movies: store.getState().movies
+    });
+});
 
 export default store
