49e8c31336fd4c5cbfb25d784bc8fef0270eb73a
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-01-22 22:52:54 +0100

    add import

--- movie-world/src/store/store.js
+++ movie-world/src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/rootReducer'
 import thunk from 'redux-thunk';
-
+import { loadState, saveState } from '../handlers/localStorage'
 const persistedState = loadState();
 
 const store = createStore(rootReducer, persistedState, applyMiddleware(thunk))
